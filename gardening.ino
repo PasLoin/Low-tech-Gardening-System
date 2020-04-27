fb97765f94272fec124039629f368797a40a6b63
PasLoin <raphael0arnould@gmail.com>
2020-04-27 10:47:26 +0200

    Update gardening.ino

--- gardening.ino
+++ gardening.ino
@@ -1,6 +1,7 @@
-int moistureSensor = 0; //one nail goes to +5V, the other nail goes to this analogue pin
+int moistureSensor = 0; //one nail goes to voltage pin, the other nail goes to this analogue pin
 int moisture_val;
-
+const int voltage = 8;             // the voltage nail is on pin D8
+const int dodo = 5000;  // délais entre prise de mesure
 
 // Setup runs on boot
 void setup() {
@@ -8,8 +9,14 @@ Serial.begin(9600); //open serial port
 }
 // Loop is the main application loop
 void loop() {
+digitalWrite(voltage, HIGH);
+delay(500);
 moisture_val = analogRead(moistureSensor); // read the value from the nails
 Serial.print("moisture sensor reads ");
 Serial.println( moisture_val ); // print the moisture level to the serial monitor
-delay(500);
+digitalWrite(voltage, LOW);
+Serial.print("Sensor is off for ");
+Serial.print(dodo);
+Serial.println(" millisecondes");
+delay(dodo);
 }
